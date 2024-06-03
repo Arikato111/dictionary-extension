5193eccda5ffa816ca02157b5791442afc1fc324
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-06-04 02:05:44 +0700

    setup name and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "wxt";
 import react from "@vitejs/plugin-react";
-import background from "./entrypoints/background";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    name: "The Cambridge Dictionary",
+    description: "searching word in Cambridge dictionary",
     permissions: ["tabs", "contextMenus"],
   },
   vite: () => ({
